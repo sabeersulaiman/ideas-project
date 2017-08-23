57b52131441e5cc1e71eee05211f2efea35d870f
Sabeer Sulaiman <sabeer@fantacode.com>
2017-08-24 02:22:32 +0530

    Env Test

--- Masjid/SQLDisposible.cs
+++ Masjid/SQLDisposible.cs
@@ -13,12 +13,13 @@ namespace MasjidPro
 		public SQLDisposible()
 		{
 			//string envvariable = Environment.GetEnvironmentVariable("SQLCONNSTR_localdb");
-			string envvariable = @"Server=tcp:idea-addin.database.windows.net,1433;Initial Catalog=idea-sql;Persist Security Info=False;User ID=sabeersulaiman;Password=Db#128$AbEeR;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //@"Server = DESKTOP-JOFGSH2; Database = IdeaDB; Trusted_Connection = True;";
 #if DEBUG
+			string envvariable = @"Server = DESKTOP-JOFGSH2; Database = IdeaDB; Trusted_Connection = True;"; //@"Server=tcp:idea-addin.database.windows.net,1433;Initial Catalog=idea-sql;Persist Security Info=False;User ID=sabeersulaiman;Password=Db#128$AbEeR;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 			Connection = new SqlConnection(envvariable);
 			Console.WriteLine("Debug Key Taken");
 #else
-            Connection = new SqlConnection(envvariable);
+            string envvariable = Environment.GetEnvironmentVariable("MSSQL_CONNECTION");
+			Connection = new SqlConnection(envvariable);
 			Console.WriteLine("Debug Key Taken");
 #endif
 		}
